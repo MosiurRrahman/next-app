9b7753448c8445934c1ef88a7baf63af545536b4
MosiurRrahman <mosiurrahman595@gmail.com>
2022-12-06 11:05:28 +0600

    header siderba mportr

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import Sidebar from "../components/com+mon/sidebar";
 // import Banner1 from "../components/banner/Banner1";
 // import About1 from "../components/about/About1";
 // import HomeBooking1 from "../components/booking/HomeBooking1";
-// import Footer from "../components/footer/Footer";
 // import HomeGallary1 from "../components/gallary/HomeGallary1";
-// import Header from "../components/header/Header";
 // import HomeNews1 from "../components/news/HomeNews1";
 // import InstagramFeed1 from "../components/nstagramFeed/InstagramFeed1";
 // import HomePricePlan from "../components/pricing-plan/HomePricePlan";
 // import Service1 from "../components/service/Service1";
 // import HomeTestimonial1 from "../components/testimonial/HomeTestimonial1";
 // import HomeVideo1 from "../components/video/HomeVideo1";
-// import Sidebar from "../components/common/sidebar";
 
 
 
