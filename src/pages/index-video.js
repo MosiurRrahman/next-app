f49f5132d8e5fd3eb7a9c8d003aeabebb336fd8e
MosiurRrahman <mosiurrahman595@gmail.com>
2022-12-06 12:49:50 +0600

    remove show case and indec video serviee 1 component

--- src/pages/index-video.js
+++ src/pages/index-video.js
@@ -9,7 +9,6 @@ import HomeNews1 from "../components/news/HomeNews1";
 import InstagramFeed1 from "../components/nstagramFeed/InstagramFeed1";
 import Partner1 from "../components/partnar/Partner1";
 import HomePricePlan from "../components/pricing-plan/HomePricePlan";
-import Service1 from "../components/service/Service1";
 import Services1 from "../components/services/Services1";
 import HomeTestimonial1 from "../components/testimonial/HomeTestimonial1";
 import HomeVideo1 from "../components/video/HomeVideo1";
