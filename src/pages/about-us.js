af8ceb8839472eaa3a1eee99b5d1fe6d28fdd5de
MosiurRrahman <mosiurrahman595@gmail.com>
2022-12-09 18:16:47 +0600

    dabouts

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -4,7 +4,7 @@ import Breadcrumb from '../components/breadcrumb/Breadcrumb'
 import InstagramFeed1 from '../components/nstagramFeed/InstagramFeed1';
 import Partner1 from '../components/partnar/Partner1';
 import Services2 from '../components/services/Services2';
-import AboutTestimonial from '../components/testimonial/aboutTestimonial';
+import AboutTestimonial from '../components/testimonial/AboutTestimonial';
 import Layout from "../layout/Layout";
 function AboutUs() {
   return (
