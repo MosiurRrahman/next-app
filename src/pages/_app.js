12875b80096896f4bfcb4f882a23efdf06f77788
MosiurRrahman <mosiurrahman595@gmail.com>
2022-12-20 12:57:17 +0600

    restho add bootstrap js link

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
 // import all css
+import { useEffect } from "react";
 import "../styles/main.css"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("../../public/js/bootstrap.bundle.min.js");
+  });
   return <Component {...pageProps} />
 }
 
