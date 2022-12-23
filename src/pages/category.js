158fe73d47310d47593f650da62325536f6bf72f
MosiurRrahman <mosiurrahman595@gmail.com>
2022-12-23 11:33:21 +0600

    category foot full dynamic

--- src/pages/category.js
+++ src/pages/category.js
@@ -21,10 +21,11 @@ function Category() {
                 <h4 className="title">Category:</h4>
                 <ul className="nav nav-tabs" id="myTab" role="tablist">
                   {
-                    [...categoryCard].map( ( key ) => {
+                    [...categoryCard].map( ( key, index ) => {
+                   
                       return(
-                        <li key={key.id} className="nav-item" role="presentation">
-                          <button className="nav-link" id="vegetarian-tab" data-bs-toggle="tab" data-bs-target={`#${ key[1]}`} type="button" role="tab" aria-controls={`#${ key[1]}`} aria-selected="false"><span>{key[0]}</span><span><i className="bi bi-arrow-right" /></span></button>
+                        <li key={key[1]} className="nav-item" role="presentation">
+                          <button className={index===0?"active nav-link":"nav-link"} id="vegetarian-tab" data-bs-toggle="tab" data-bs-target={`#${ key[1]}`} type="button" role="tab" aria-controls={`#${ key[1]}`} aria-selected="false"><span>{key[0]}</span><span><i className="bi bi-arrow-right" /></span></button>
                         </li>
                       )
                     })
@@ -34,99 +35,40 @@ function Category() {
             </div>
             <div className="col-lg-9">
               <div className="tab-content">
-                
-                <div className="tab-pane" id="seafood" role="tabpanel" aria-labelledby="seafood-tab">
-                  <div className="food-category-wrap">
-                    <div className="row g-4">
-                      <div className="divider" />
-                      <div className="col-lg-6 col-md-6">
-                        <div className="category-item">
-                          <div className="icon">
-                            <img className="img-fluid" src="assets/images/bg/sea-food-11.png" alt="" />
-                          </div>
-                          <div className="category-content">
-                            <h4><a href="shop-details.html">Pasta Mixed Prawn</a></h4>
-                            <p>Complete account of the system.</p>
-                            <div className="price">
-                              <h5>$20 <span>Only</span></h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="category-item">
-                          <div className="icon">
-                            <img className="img-fluid" src="assets/images/bg/sea-food-12.png" alt="" />
-                          </div>
-                          <div className="category-content">
-                            <h4><a href="shop-details.html">Shrimp Mashala</a></h4>
-                            <p>Complete account of the system.</p>
-                            <div className="price">
-                              <h5>$20 <span>Only</span></h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="category-item">
-                          <div className="icon">
-                            <img className="img-fluid" src="assets/images/bg/sea-food-13.png" alt="" />
-                          </div>
-                          <div className="category-content">
-                            <h4><a href="shop-details.html">Shrimp Noodles</a></h4>
-                            <p>Complete account of the system.</p>
-                            <div className="price">
-                              <h5>$15 <span>Only</span></h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="category-item">
-                          <div className="icon">
-                            <img className="img-fluid" src="assets/images/bg/sea-food-14.png" alt="" />
-                          </div>
-                          <div className="category-content">
-                            <h4><a href="shop-details.html">Salmon Fish</a></h4>
-                            <p>Complete account of the system.</p>
-                            <div className="price">
-                              <h5>$15 <span>Only</span></h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="category-item">
-                          <div className="icon">
-                            <img className="img-fluid" src="assets/images/bg/sea-food-15.png" alt="" />
-                          </div>
-                          <div className="category-content">
-                            <h4><a href="shop-details.html">Broccoli Item</a></h4>
-                            <p>Complete account of the system.</p>
-                            <div className="price">
-                              <h5>$13 <span>Only</span></h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-lg-6 col-md-6">
-                        <div className="category-item">
-                          <div className="icon">
-                            <img className="img-fluid" src="assets/images/bg/sea-food-16.png" alt="" />
-                          </div>
-                          <div className="category-content">
-                            <h4><a href="shop-details.html">Tubacco Fish</a></h4>
-                            <p>Complete account of the system.</p>
-                            <div className="price">
-                              <h5>$13 <span>Only</span></h5>
-                            </div>
-                          </div>
+                {
+                  [...categoryCard].map( ( key,index ) => {
+                    console.log(key);
+                    return(
+                      <div key={key[1]} className={index === 0 ? "active tab-pane":"tab-pane"} id={`${ key[1]}`} role="tabpanel" aria-labelledby="seafood-tab">
+                      <div className="food-category-wrap">
+                        <div className="row g-4">
+                          <div className="divider" />
+                          {
+                            categodyCardData.filter( (data) => (data.categorySlug == key[1] )).map( ( value ) => {
+                              return(
+                                <div key={value.id} className="col-lg-6 col-md-6">
+                                  <div className="category-item">
+                                    <div className="icon">
+                                      <img className="img-fluid" src={ value.image } alt="" />
+                                    </div>
+                                    <div className="category-content">
+                                      <h4><a href="shop-details.html">{ value.title } </a></h4>
+                                      <p>{ value.description }</p>
+                                      <div className="price">
+                                        <h5>${ value.price } <span>Only</span></h5>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>    
+                              )
+                            })
+                          }
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-
+                    )
+                  })
+                }
               </div>
             </div>
           </div>
