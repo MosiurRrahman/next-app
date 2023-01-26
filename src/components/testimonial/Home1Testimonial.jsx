7f2b589e9bf01b3acfc65d0023abb6370124694b
MosiurRrahman <mosiurrahman595@gmail.com>
2023-01-26 15:11:42 +0600

    dadjaoidja

--- src/components/testimonial/Home1Testimonial.jsx
+++ src/components/testimonial/Home1Testimonial.jsx
@@ -2,75 +2,76 @@ import React from "react";
 
 function Home1testimonial() {
   return (
-    // <div className="section-common testimonial-three-section pt-120">
-    //   <div className="row">
-    //     <div className="section-title-three text-center">
-    //       <div className="water-text">Testimonial</div>
-    //       <h2>Testimonial</h2>
-    //     </div>
-    //   </div>
-    //   <div className="row g-4">
-    //     <div className="col-12">
-    //       <div className="testimonial-three">
-    //         <div className="testimonial-three-desig">
-    //           <img src="assets/images/testimonial/testi3-1.png" alt="image" />
-    //           <h5>Leslie Alexander</h5>
-    //           <span>Customer</span>
-    //         </div>
-    //         <div className="testimonial-three-desc">
-    //           <p>
-    //             I work with Alguneb Johnl on many projects, he always tolda
-    //             autonai exceeds my expectations with his quality work and
-    //             fastestopa Sedanl convallis magna vitae eros sollicitudin
-    //             pulvinar service, very smoothai and simple communication.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="col-12">
-    //       <div className="testimonial-three">
-    //         <div className="testimonial-three-desig">
-    //           <img src="assets/images/testimonial/testi3-2.png" alt="image" />
-    //           <h5>Samuel Jacoba</h5>
-    //           <span>Customer</span>
-    //         </div>
-    //         <div className="testimonial-three-desc">
-    //           <p>
-    //             I work with Alguneb Johnl on many projects, he always tolda
-    //             autonai exceeds my expectations with his quality work and
-    //             fastestopa Sedanl convallis magna vitae eros sollicitudin
-    //             pulvinar service, very smoothai and simple communication.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="col-12">
-    //       <div className="testimonial-three">
-    //         <div className="testimonial-three-desig">
-    //           <img src="assets/images/testimonial/testi3-3.png" alt="image" />
-    //           <h5>Josephon Wyatt</h5>
-    //           <span>Customer</span>
-    //         </div>
-    //         <div className="testimonial-three-desc">
-    //           <p>
-    //             I work with Alguneb Johnl on many projects, he always tolda
-    //             autonai exceeds my expectations with his quality work and
-    //             fastestopa Sedanl convallis magna vitae eros sollicitudin
-    //             pulvinar service, very smoothai and simple communication.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="row mt-40">
-    //     <div className="col-12 text-center">
-    //       <button className="eg-btn btn--primary btn--lg load-more-btn">
-    //         Load More
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
-    <></>
+    <>
+      <div className="section-common testimonial-three-section pt-120">
+        <div className="row">
+          <div className="section-title-three text-center">
+            <div className="water-text">Testimonial</div>
+            <h2>Testimonial</h2>
+          </div>
+        </div>
+        <div className="row g-4">
+          <div className="col-12">
+            <div className="testimonial-three">
+              <div className="testimonial-three-desig">
+                <img src="assets/images/testimonial/testi3-1.png" alt="image" />
+                <h5>Leslie Alexander</h5>
+                <span>Customer</span>
+              </div>
+              <div className="testimonial-three-desc">
+                <p>
+                  I work with Alguneb Johnl on many projects, he always tolda
+                  autonai exceeds my expectations with his quality work and
+                  fastestopa Sedanl convallis magna vitae eros sollicitudin
+                  pulvinar service, very smoothai and simple communication.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="testimonial-three">
+              <div className="testimonial-three-desig">
+                <img src="assets/images/testimonial/testi3-2.png" alt="image" />
+                <h5>Samuel Jacoba</h5>
+                <span>Customer</span>
+              </div>
+              <div className="testimonial-three-desc">
+                <p>
+                  I work with Alguneb Johnl on many projects, he always tolda
+                  autonai exceeds my expectations with his quality work and
+                  fastestopa Sedanl convallis magna vitae eros sollicitudin
+                  pulvinar service, very smoothai and simple communication.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="testimonial-three">
+              <div className="testimonial-three-desig">
+                <img src="assets/images/testimonial/testi3-3.png" alt="image" />
+                <h5>Josephon Wyatt</h5>
+                <span>Customer</span>
+              </div>
+              <div className="testimonial-three-desc">
+                <p>
+                  I work with Alguneb Johnl on many projects, he always tolda
+                  autonai exceeds my expectations with his quality work and
+                  fastestopa Sedanl convallis magna vitae eros sollicitudin
+                  pulvinar service, very smoothai and simple communication.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-40">
+          <div className="col-12 text-center">
+            <button className="eg-btn btn--primary btn--lg load-more-btn">
+              Load More
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
