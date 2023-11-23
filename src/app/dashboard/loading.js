2c3d4a787e1099204301d0a8af3a3b24893b9766
MosiurRrahman <mosiurrahman595@gmail.com>
2023-11-23 13:11:56 +0600

    alpoxn

--- src/app/dashboard/loading.js
+++
@@ -1,8 +0,0 @@
-
-export default function Loading() {
-  
-    // Or a custom loading skeleton component
-    return <div className='loading-component'>
-      <div className="loader"></div>
-    </div>
-  }
