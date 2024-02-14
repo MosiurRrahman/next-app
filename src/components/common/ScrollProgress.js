d7b446ad08ccf70d31172fda05589508d1e568e8
MosiurRrahman <mosiurrahman595@gmail.com>
2024-02-14 12:30:16 +0600

    sc

--- src/components/common/ScrollProgress.js
+++ src/components/common/ScrollProgress.js
@@ -1,28 +1,32 @@
-"use client";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ScrollProgress = () => {
   const [visible, setVisible] = useState(false);
 
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 800) {
-      setVisible(true);
-    } else if (scrolled <= 100) {
-      setVisible(false);
-    }
-  };
+  useEffect(() => {
+    const toggleVisible = () => {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 800) {
+        setVisible(true);
+      } else if (scrolled <= 100) {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisible);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once on mount
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
-      /* you can also use 'auto' behaviour
-         in place of 'smooth' */
     });
   };
 
-  window.addEventListener("scroll", toggleVisible);
   return (
     <div
       className={`circle-container ${visible ? "active" : ""}`}
