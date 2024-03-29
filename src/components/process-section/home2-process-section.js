db4a9bf50bd50148112d7a040b9a18ddf76e5b30
MosiurRrahman <mosiurrahman595@gmail.com>
2024-03-29 16:53:36 +0600

    dasnlkd

--- src/components/process-section/home2-process-section.js
+++ src/components/process-section/home2-process-section.js
@@ -1,7 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const Home2ProcessSection = () => {
     useEffect(() => {
